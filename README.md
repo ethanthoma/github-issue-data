1eecb854807e0b8625608925b002d2ac7be58538
Ethan Thoma <ethanthoma@gmail.com>
2024-02-29 23:34:03 -0800

    initial push, basic fetching of GitHub API

---
+++ README.md
@@ -0,0 +1,11 @@
+# Fetching GitHub issue data
+
+## TODO
+- fetch user information
+- fix flitering criteria for low star repos (no bug issues)
+- refactor
+
+## Install + Run
+
+You will have to have `nix` installed with experimental features for flakes.
+Run `nix run` to run the program.
---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+    description = "A basic gomod2nix flake";
+
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        flake-utils.url = "github:numtide/flake-utils";
+        gomod2nix = {
+            url = "github:nix-community/gomod2nix";
+            inputs = {
+                nixpkgs.follows = "nixpkgs";
+                flake-utils.follows = "flake-utils";
+            };
+        };
+    };
+
+    outputs = { self, nixpkgs, flake-utils, gomod2nix }:
+    (flake-utils.lib.eachDefaultSystem 
+        (system: 
+        let
+            pkgs = nixpkgs.legacyPackages.${system};
+
+            callPackage = pkgs.darwin.apple_sdk_11_0.callPackage or pkgs.callPackage;
+        in {
+            packages.default = callPackage ./. {
+                inherit (gomod2nix.legacyPackages.${system}) buildGoApplication;
+            };
+            devShells.default = callPackage ./shell.nix {
+                inherit (gomod2nix.legacyPackages.${system}) mkGoEnv gomod2nix;
+            };
+        }
+        )
+    );
+}
